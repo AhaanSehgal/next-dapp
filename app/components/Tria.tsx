4da5720df9cd1f0800e3f3f33e88a9dced52be03
AhaanSehgal <ahaan@threely.io>
2023-12-13 15:57:59 +0530

    window

--- app/components/Tria.tsx
+++ app/components/Tria.tsx
@@ -2,22 +2,35 @@
 "use client"
 import dynamic from 'next/dynamic'
 import "authenticate-test-2/dist/index.css"
+import { useState } from 'react'
 const Application = dynamic(
     () => import("authenticate-test-2"),
     { ssr: false }
 )
 
 const Tria = () => {
+
+    const [defined, setDefined] = useState(false)
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setDefined(true)
+        }
+    }, [])
+
     return (
-        <Application
-            logo="https://www.stackos.io/stackos-logo.svg"
-            dappName="Stack OS"
-            dappDomain={window?.parent?.origin}
-            uiType={"yes"}
-            primaryColor="#AAFF00"
-            defaultChain="FUSE"
-            supportedChains={["FUSE", "POLYGON"]}
-        />
+        <>
+            {defined &&
+                <Application
+                    logo="https://www.stackos.io/stackos-logo.svg"
+                    dappName="Stack OS"
+                    dappDomain={window?.parent?.origin}
+                    uiType={"yes"}
+                    primaryColor="#AAFF00"
+                    defaultChain="FUSE"
+                    supportedChains={["FUSE", "POLYGON"]}
+                />}
+        </>
     )
 }
 
